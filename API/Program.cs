04f1f69f2a697d64624abbe5c3132abbc64fd987
bishwa-prakash <sahoo.bishwaprakash2@gmail.com>
2023-12-17 18:31:45 +0530

    End of section 8

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,4 @@
-using System.Text;
+
 using API.Data;
 using API.Extensions;
 using API.Interfaces;
@@ -32,4 +32,18 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+using var scope=app.Services.CreateScope();
+var services=scope.ServiceProvider;
+try
+{
+    var context=services.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedUsers(context);
+
+}
+catch(Exception ex)
+{
+    var logger=services.GetService<ILogger<Program>>();
+    logger.LogError(ex,"An error occured during migration");
+}
 app.Run();
